6d3d29adb0612c5f3f35f4fe3cb363195c84ba10
Lee-Jae-Ik <leeji0922@ontacthealth.com>
2022-10-31 18:51:24 +0900

    gjgj

---
+++ src/main/java/com/lji/portal/actuator/ConcurrentTransactionCountInterceptor.java
@@ -0,0 +1,15 @@
+package com.lji.portal.actuator;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import sun.management.counter.Counter;
+
+/**
+ * ConcurrentTransactionCountInterceptor
+ *
+ * @author Lee-Jae-Ik
+ * @version 0.1
+ * @see
+ * @since 2022-10-31
+ */
+public class ConcurrentTransactionCountInterceptor extends HandlerInterceptorAdapter {
+}
